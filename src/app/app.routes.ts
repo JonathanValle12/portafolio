089b5693e52dff7e05f8bf7c838c5a03f626a088
Jonathan12V <2023_jonathan.valle@iticbcn.cat>
2024-04-11 13:07:49 +0200

    añadir iconos github y linkedin y componente de error 404

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes, RouterModule } from '@angular/router';
 import { ModuleWithProviders } from '@angular/core';
 import { InicioComponent } from './components/inicio/inicio.component';
+import { ErrorComponent } from './components/error/error.component';
 
 export const routes: Routes = [
 
     { path: "", component: InicioComponent},
+    { path: "**", component: ErrorComponent}
 ];
 
 export const routing: ModuleWithProviders<any> = RouterModule.forRoot(routes);
